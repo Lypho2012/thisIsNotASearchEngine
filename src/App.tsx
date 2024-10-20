2b475ab357bd4ca8633676b6210c0d14f6e5b62e
Lypho2012 <ioi2020acsl@gmail.com>
2024-10-19 17:05:20 -0700

    working on suggestion div

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css';
+import Home from './pages/Home.tsx'
+import WhySkyBlue from './pages/WhySkyBlue.tsx'
+import ImHappy from './pages/ImHappy.tsx'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter >
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path={"/"+encodeURIComponent("Why is the sky blue")} element={<WhySkyBlue/>}></Route>
+          <Route path={"/"+encodeURIComponent("I think I'm happy")} element={<ImHappy/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,31 @@
-import React, {useState} from 'react'
-import {useNavigate} from "react-router-dom"
+import React, { useEffect, useState } from 'react'
+import {Link} from 'react-router-dom'
 import './Home.css'
 
 class Node {
+    children: Map<string, any>
     constructor() {
         this.children = new Map()
     }
-    add(word) {
+    add(word: string) {
         let cur = this
         for (let i=0; i<word.length; i++) {
             cur.children.set(word[i].toLowerCase(),new Node())
             cur = cur.children.get(word[i].toLowerCase())
         }
     }
-    search(word) {
+    search(word: string) {
         let cur = this
         for (let i=0; i<word.length; i++) {
             if (!cur.children.get(word[i].toLowerCase())) return []
             cur = cur.children.get(word[i].toLowerCase())
         }
-        let results = []
+        let results: string[] = []
         for (let [child,val] of cur.children) {
             let curResults = val.getWords()
             for (let result of curResults) {
                 results.push(word+child+result)
+                console.log(word)
                 console.log(word+child+result)
             }
         }
@@ -31,7 +33,7 @@ class Node {
     }
     getWords() {
       if (this.children.size == 0) return [""]
-      let result = []
+      let result: string[] = []
       for (let [child,val] of this.children) {
         let cur = val.getWords()
         for (let word of cur) {
@@ -53,29 +55,41 @@ function Home() {
     const SEARCH_PROMPTS = ["Why is the sky blue", "I think I'm happy", "Crowdstrike"]
     var trieRoot = createTrie(SEARCH_PROMPTS)
     const [searchTerm, setSearchTerm] = useState("")
-    const [suggested, setSuggested] = useState([])
+    const [suggested, setSuggested] = useState<string[]>([])
     const handleInputChange = (event) => {
         setSearchTerm(event.target.value);
         // display suggested searches
+        console.log(event)
         setSuggested(trieRoot.search(searchTerm))
-        console.log(suggested)
     };
     const handleSubmit = (event) => {
         event.preventDefault();
         // navigate to page
     }
-    const navigate = (page) => {
-        useNavigate(encodeURIComponent(page))
-    }
+    document.addEventListener("click", function(event) {
+        if (event.target.id == "search-bar-text") {
+            var searchBar = document.getElementById("search-bar")
+            if (searchBar != null) {
+                searchBar.focus();
+                console.log(document.activeElement)
+            }
+        } else {
+            var searchBar = document.getElementById("search-bar")
+            if (searchBar != null) {
+                searchBar.blur();
+            }
+        }
+    })
+    
     return (
     <div id="Google-page">
         <div id="Google-header">
-            <div id="big-g-letter" class="Google-letter">G</div>
-            <div id="first-o-letter" class="Google-letter">o</div>
-            <div id="second-o-letter" class="Google-letter">o</div>
-            <div id="little-g-letter" class="Google-letter">g</div>
-            <div id="l-letter" class="Google-letter">l</div>
-            <div id="e-letter" class="Google-letter">e</div>
+            <div id="big-g-letter" className="Google-letter">G</div>
+            <div id="first-o-letter" className="Google-letter">o</div>
+            <div id="second-o-letter" className="Google-letter">o</div>
+            <div id="little-g-letter" className="Google-letter">g</div>
+            <div id="l-letter" className="Google-letter">l</div>
+            <div id="e-letter" className="Google-letter">e</div>
         </div>
         <div id="search-bar-div">
             <div id="search-bar">
@@ -89,16 +103,26 @@ function Home() {
                 onChange={handleInputChange}
                 />
             </div>
+            <div id="suggestions">
+            {
+                suggested.map((suggestion) => {
+                    return (
+                        <div className="suggestion">
+                            <svg className="suggestion-search-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20px">
+                                <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+                            </svg>
+                            <Link 
+                                to={"/"+encodeURIComponent(suggestion)}
+                                className="suggestion-link">
+                                    {suggestion}
+                            </Link>
+                        </div>
+                    )
+                })
+            }
+            </div>
         </div>
-        <div>
-        {
-            suggested.map((suggestion) => {
-                return (
-                    <div onClick={suggestion}>{suggestion}</div>
-                )
-            })
-        }
-        </div>
+        
     </div>
     )
 }
