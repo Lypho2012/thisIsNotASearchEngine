4c52102eb85cf37f2fcbc7e3585d2d5de806da22
Lypho2012 <ioi2020acsl@gmail.com>
2024-10-22 22:46:45 -0700

    add about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import './About.css'
+import { Link } from 'react-router-dom'
 
 function About() {
   return (
-    <div>About</div>
+    <div id="about-page">
+        <div id="about-heading">This is NOT a search engine!</div>
+        <div id="about-body">Were you fooled? This was just a project that I took on for fun to fill up with some silly ideas.</div>
+        <Link to="/" id="about-back-link">Back</Link>
+    </div>
   )
 }
 
