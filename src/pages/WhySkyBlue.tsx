d741f8f03c8de391cc60cb4f946e994e2ad37884
Lypho2012 <ioi2020acsl@gmail.com>
2024-10-28 09:24:17 -0700

    add search category component

--- src/pages/WhySkyBlue.tsx
+++ src/pages/WhySkyBlue.tsx
@@ -2,8 +2,10 @@ import React, { useEffect } from 'react'
 import "./WhySkyBlue.css"
 import SearchBar from './components/SearchBar.tsx'
 import Google from './components/Google.tsx'
+import SearchCategories from './components/SearchCategories.tsx'
 
 function WhySkyBlue() {
+  const search_results = [{link_name:"",link:"",summary:""}]
   useEffect(() => {
     let google_letters = document.getElementsByClassName("Google-letter")
     for (let google_letter of google_letters) {
@@ -19,6 +21,10 @@ function WhySkyBlue() {
             <Google/>
           </div>
           <SearchBar prevSearchTerm="why is the sky blue"/>
+        </div>
+        <SearchCategories/>
+        <div id="bluesky-search-result-list-div">
+          {}
         </div>
       </div>
       <div id="sun"></div>
---
+++ src/pages/components/SearchCategories.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+import "./SearchCategories.css"
+
+function SearchCategories() {
+    const categories = ["search-category-all","search-category-images","search-category-videos","search-category-shopping","search-category-news","search-category-forums","search-category-web"]
+    const [selectedCategory, setSelectedCategory] = useState("all")
+    const handleChooseCategory = (event) => {
+        setSelectedCategory(event.target.innerText.toLowerCase())
+    }
+    useEffect(() => {
+        for (let category of categories) {
+            let button = document.getElementById(category);
+            if (category == "search-category-"+selectedCategory) {
+                if (button) {
+                    button.style.borderBottom = "solid 3px"
+                }
+            } else {
+                if (button) {
+                    button.style.borderBottom = "none"
+                }
+            }
+        }
+    })
+    return (
+        <div id="search-categories">
+            <button id="search-category-all" className="search-category" onClick={handleChooseCategory}>All</button>
+            <button id="search-category-images" className="search-category" onClick={handleChooseCategory}>Images</button>
+            <button id="search-category-videos" className="search-category" onClick={handleChooseCategory}>Videos</button>
+            <button id="search-category-shopping" className="search-category" onClick={handleChooseCategory}>Shopping</button>
+            <button id="search-category-news" className="search-category" onClick={handleChooseCategory}>News</button>
+            <button id="search-category-forums" className="search-category" onClick={handleChooseCategory}>Forums</button>
+            <button id="search-category-web" className="search-category" onClick={handleChooseCategory}>Web</button>
+        </div>
+    )
+}
+
+export default SearchCategories
