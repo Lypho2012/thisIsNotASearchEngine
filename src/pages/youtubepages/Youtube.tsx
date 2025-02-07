1a00fd5c62a6f5017f1ec1fb74ae145e2ffb8e60
Cindy Zhang <xinyuz62@uci.edu>
2025-02-06 18:00:58 -0800

    add youtube sidebar

--- src/pages/youtubepages/Youtube.tsx
+++ src/pages/youtubepages/Youtube.tsx
@@ -3,11 +3,12 @@ import "./Youtube.css"
 import HomeSVG from "./home.svg"
 import MenuSVG from "./menu.svg"
 import SearchSVG from "./search.svg"
-import XSVG from "./x.svg"
 import VoiceSVG from "./mic-filled.svg"
 import AddSVG from "./add.svg"
 import NotificationSVG from "./bell.svg"
 import ProfileSVG from "./account.svg"
+import ShortsSVG from "./shorts.svg"
+import YoutubeMusicSVG from "./youtube-music.svg"
 
 function Youtube() {
     const [searchTerm, setSearchTerm] = useState("")
@@ -36,7 +37,6 @@ function Youtube() {
                     value={searchTerm}
                     onChange={handleInputChange}/>
                     <button id="search-button"><img src={SearchSVG} alt="search" id="search-icon"/></button>
-                    <button id="search-x-button" onClick={handleXInput}><img src={XSVG} alt="clear" id="x-icon"/></button>
                     <button id="voice-button"><img src={VoiceSVG} alt="voice" id="voice-icon"/></button>
                 </div>
                 <div id="youtube-profile">
@@ -48,8 +48,47 @@ function Youtube() {
                     <button id="profile-button"><img src={ProfileSVG} alt="profile" id="profile-icon"/></button>
                 </div>
             </div>
-
-            <div id="youtube-sidebar">
+            <div id="horizontal-youtube-main-div">
+                <div id="youtube-sidebar">
+                    <button className="youtube-sidebar-button">
+                        <img src={HomeSVG} alt="home" className="youtube-sidebar-icon"/>
+                        <div>Home</div>
+                    </button>
+                    <button className="youtube-sidebar-button">
+                        <img src={ShortsSVG} alt="shorts" className="youtube-sidebar-icon"/>
+                        Shorts
+                    </button>
+                    <button className="youtube-sidebar-button">
+                        <img src={require("./subscriptions.png")} alt="subscriptions" className="youtube-sidebar-icon"/>
+                        Subscriptions
+                    </button>
+                    <button className="youtube-sidebar-button">
+                        <img src={YoutubeMusicSVG} alt="youtube-music" className="youtube-sidebar-icon"/>
+                        Youtube Music
+                    </button>
+                    <button className="youtube-sidebar-button">
+                        <img src={ProfileSVG} alt="you" className="youtube-sidebar-icon"/>
+                        You
+                    </button>
+                    <button className="youtube-sidebar-button">
+                        <img src={require("./download.png")} alt="download" className="youtube-sidebar-icon"/>
+                        Downloads
+                    </button>
+                </div>
+                <div id="main-content">
+                    <div id="tags"></div>
+                    <div className="rows">
+                        <div className="row">
+                            <div className="video">
+                                <div className="thumbnail"></div>
+                                <div className="channel-pfp"></div>
+                                <div className="video-name"></div>
+                                <div className="channel-name"></div>
+                                <div className="stats"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
