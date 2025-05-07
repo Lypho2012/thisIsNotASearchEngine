7a033ed0bd257b54ebdd4bba3a5c917e7a6c7556
Cindy Zhang <xinyuz62@uci.edu>
2025-05-07 10:08:47 -0700

    get images from google photos

--- backend/collage.py
+++ backend/collage.py
@@ -8,7 +8,15 @@ import httpx
 import asyncio
 import json
 
+from dotenv import load_dotenv
 import os
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+import time
 
 class GoogleAuth(httpx.Auth):
     def __init__(self, credentials):
@@ -45,13 +53,89 @@ def get_avg_color(image,x,y):
 def is_similar_color(color1, color2):
     return np.linalg.norm(color1 - color2) < 5
 
+def select_all_images(url):
+    query = os.getenv("GOOGLE_PHOTOS_QUERY")
+
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(5)
+    driver.get(url)
+
+    # search for images
+    while True:
+        try:
+            search_bar = driver.find_element(By.CSS_SELECTOR,f"[aria-label='Search your photos and albums']")
+            search_bar.send_keys(query+" jpg")
+            search_bar.send_keys(Keys.ENTER)
+            break
+        except:
+            continue
+
+    # select first image
+    while True:
+        try:
+            first_image = driver.find_element(By.CSS_SELECTOR,f"[role='checkbox']")
+            first_image.click()
+            break
+        except:
+            continue
+
+    # scroll to last image
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    driver.implicitly_wait(5)
+
+    # shift click last image
+    while True:
+        try:
+            last_image = driver.find_elements(By.CSS_SELECTOR,f"[role='checkbox']")[-1]
+            ActionChains(driver) \
+                .key_down(Keys.SHIFT) \
+                .click(last_image) \
+                .key_up(Keys.SHIFT) \
+                .perform()
+            break
+        except:
+            continue
+
+    # click done
+    # while True:
+    #     try:
+    #         done_button = driver.find_element_by_xpath("//div[text()='Done']")
+    #         done_button.click()
+    #         break
+    #     except:
+    #         continue
+
 async def create_collage(background_image):
     creds = authenticate_google("backend/google_photos_picker_secret.json",["https://www.googleapis.com/auth/photospicker.mediaitems.readonly"])
     client = httpx.AsyncClient(auth=GoogleAuth(creds))
+
+    # start a session
     google_photos = await client.post("https://photospicker.googleapis.com/v1/sessions")
-    print(json.loads(google_photos.content.decode('utf-8'))["pickerUri"])
-    
-    return None
+    session_info = json.loads(google_photos.content.decode('utf-8'))
+    url = session_info["pickerUri"]
+    session_id = session_info["id"]
+    print(session_info)
+
+    # check when session is finished
+    can_continue = False
+    while not can_continue:
+        time.sleep(5)
+        google_photos = await client.get(f"https://photospicker.googleapis.com/v1/sessions/{session_id}")
+        session_info = json.loads(google_photos.content.decode('utf-8'))
+        can_continue = session_info["mediaItemsSet"]
+
+    # get selected photos
+    picked = []
+    nextPageToken = ""
+    while True:
+        google_photos = await client.get(f"https://photospicker.googleapis.com/v1/mediaItems?sessionId={session_id}&pageToken={nextPageToken}")
+        session_info = json.loads(google_photos.content.decode('utf-8'))
+        picked += [photo["mediaFile"]["baseUrl"] for photo in session_info["mediaItems"]]
+        if "nextPageToken" in session_info:
+            nextPageToken = session_info["nextPageToken"]
+        else:
+            break
+    print(len(picked))
 
     # create image
     res_image = Image.new(mode="RGBA", size=background_image.size, color=(255,255,255,0))
