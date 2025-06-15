c973db446c07eec4753bdd81257bfe51b7d1cdf3
Cindy Zhang <xinyuz62@uci.edu>
2025-06-14 22:13:16 -0700

    change depth rendering logic

--- backend/point_cloud.py
+++ backend/point_cloud.py
@@ -5,6 +5,7 @@ import pyvista as pv
 import random
 
 RESOLUTION_STEP = 5
+THRESHOLD_DEPTH = 10
 
 def plot(points,colors):
     point_cloud = pv.PolyData(points)
@@ -38,12 +39,21 @@ def get_intersecting_point_cloud(file1, file2):
                 colors.insert(color)
     print(points,colors)
 
+def should_include(rand_depth):
+    prob = random.random()
+    return rand_depth == 0 or prob < 300/rand_depth
+
 def create_point_cloud_combined(png, foreimage):
     img1 = Image.open(f"backend/point_cloud_images/{png}")
     img2 = Image.open(f"backend/point_cloud_images/{foreimage}")
     img2 = img2.resize((img2.width,img1.height))
 
-    points = [(x,y,random.randint(0,img2.width)) for y in range(0,img1.height,RESOLUTION_STEP) for x in range(0,img1.width,RESOLUTION_STEP) if img1.getpixel((x,y))[:3] != (255,255,255)]
+    points = []
+    for y in range(0,img1.height,RESOLUTION_STEP): 
+        for x in range(0,img1.width,RESOLUTION_STEP):
+            rand_depth = random.randint(THRESHOLD_DEPTH,img2.width)
+            if img1.getpixel((x,y))[:3] != (255,255,255) and should_include(rand_depth):
+                points.append((x,y,rand_depth))
     colors = [img1.getpixel((point[0],point[1]))[:3] for point in points]
 
     min_depths = dict()
@@ -58,11 +68,12 @@ def create_point_cloud_combined(png, foreimage):
             if len(img2.getpixel((x,y))) > 3 and img2.getpixel((x,y))[3] == 0:
                 continue
             else:
+                rand_depth = rand_depth = random.randint(0,img1.width)
                 if (x,y) in min_depths:
-                    points.append((random.randint(0,min_depths[(x,y)]),y,x))
-                else:
-                    points.append((random.randint(0,img1.width),y,x))
-                colors.append(img2.getpixel((x,y))[:3])
+                    rand_depth = random.randint(0,min_depths[(x,y)])
+                if should_include(rand_depth):
+                    points.append((rand_depth,y,x))
+                    colors.append(img2.getpixel((x,y))[:3])
     plot(points,colors)
 
 create_point_cloud_combined("happyfathersdaytransparent.png","dad_foreground.png")
