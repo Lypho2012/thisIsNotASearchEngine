68f109ede240cc0255a2b734fab1586f2e411f7d
Cindy Zhang <xinyuz62@uci.edu>
2025-06-14 21:14:49 -0700

    get rid of background image in point cloud

--- backend/point_cloud.py
+++ backend/point_cloud.py
@@ -4,7 +4,7 @@ import numpy as np
 import pyvista as pv
 import random
 
-RESOLUTION_STEP = 1
+RESOLUTION_STEP = 5
 
 def plot(points,colors):
     point_cloud = pv.PolyData(points)
@@ -38,14 +38,12 @@ def get_intersecting_point_cloud(file1, file2):
                 colors.insert(color)
     print(points,colors)
 
-def create_point_cloud_combined(png, foreimage, backimage):
+def create_point_cloud_combined(png, foreimage):
     img1 = Image.open(f"backend/point_cloud_images/{png}")
     img2 = Image.open(f"backend/point_cloud_images/{foreimage}")
-    img3 = Image.open(f"backend/point_cloud_images/{backimage}")
     img2 = img2.resize((img2.width,img1.height))
-    img3 = img3.resize((img3.width,img1.height))
 
-    points = [(x,y,random.randint(0,img2.width)) for y in range(0,img1.height,RESOLUTION_STEP) for x in range(0,img1.width,RESOLUTION_STEP)]
+    points = [(x,y,random.randint(0,img2.width)) for y in range(0,img1.height,RESOLUTION_STEP) for x in range(0,img1.width,RESOLUTION_STEP) if img1.getpixel((x,y))[:3] != (255,255,255)]
     colors = [img1.getpixel((point[0],point[1]))[:3] for point in points]
 
     min_depths = dict()
@@ -58,8 +56,7 @@ def create_point_cloud_combined(png, foreimage, backimage):
     for y in range(0,img2.height,RESOLUTION_STEP): 
         for x in range(0,img2.width,RESOLUTION_STEP):
             if len(img2.getpixel((x,y))) > 3 and img2.getpixel((x,y))[3] == 0:
-                points.append((random.randint(0,img1.width),y,x))
-                colors.append(img3.getpixel((x,y))[:3])
+                continue
             else:
                 if (x,y) in min_depths:
                     points.append((random.randint(0,min_depths[(x,y)]),y,x))
@@ -68,4 +65,4 @@ def create_point_cloud_combined(png, foreimage, backimage):
                 colors.append(img2.getpixel((x,y))[:3])
     plot(points,colors)
 
-create_point_cloud_combined("happyfathersdaytransparent.png","dad_foreground.png","dad_background.png")
+create_point_cloud_combined("happyfathersdaytransparent.png","dad_foreground.png")
